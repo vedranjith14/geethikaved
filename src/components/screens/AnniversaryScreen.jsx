67bb62f0d3d8de9705f18ab18f5a04db4ce21262
vedranjith14 <phantomdeluxhero@gmail.com>
2026-03-16 20:25:20 -0400

    Update AnniversaryScreen.jsx

--- src/components/screens/AnniversaryScreen.jsx
+++ src/components/screens/AnniversaryScreen.jsx
@@ -8,7 +8,7 @@ export default function AnniversaryScreen({ onNext }) {
     const [displayedDays, setDisplayedDays] = useState(0)
 
     // Set your special date here (YYYY-MM-DD)
-    const specialDate = new Date("2024-09-11") // Change this to your actual date
+    const specialDate = new Date("2025-03-24") // Change this to your actual date
 
     useEffect(() => {
         // Calculate actual days since anniversary
